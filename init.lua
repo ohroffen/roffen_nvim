72f9e87a1de381d5489e154061c10c3c40eea2eb
roffen <fan@example.com>
2022-11-06 01:18:35 +0800

    feat(all): initial configs of nvim

---
+++ init.lua
@@ -0,0 +1,7 @@
+require('basic')
+require('plugins')
+require('colorscheme')
+-- setup for keybinding
+require('keybindings')
+-- setup for nvim-tree.lua
+-- require('nvim-tree')
---
+++ lua/keybindings.lua
@@ -0,0 +1,31 @@
+-- some leader key configs(which I think won't be used at all...
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local map = vim.api.nvim_set_keymap
+local opt = {noremap = true, silent = true}
+
+-- some basic keybindings
+
+map('v','<','<gv',opt)
+map('v','>','>gv',opt)
+
+map("n", "sv", ":vsp<CR>", opt)
+map("n", "sh", ":sp<CR>", opt)
+map("n", "sc", "<C-w>c", opt)
+map("n", "so", "<C-w>o", opt) -- close others
+
+map("n", "<A-h>", "<C-w>h", opt)
+map("n", "<A-j>", "<C-w>j", opt)
+map("n", "<A-k>", "<C-w>k", opt)
+map("n", "<A-l>", "<C-w>l", opt)
+
+-- plugins keybindings
+-- LeaderF
+vim.g.Lf_ShortcutF = '<C-p>'
+map('n','<leader>ff',':<C-U>Leaderf file --popup<CR>',opt)
+map('n','<leader>fh',':<C-U>Leaderf help --popup<CR>',opt)
+map('n','<leader>fg',':<C-U>Leaderf rg --no-messages --popup<CR>',opt)
+map('n','<leader>ft',':<C-U>Leaderf bufTag --popup<CR>',opt)
+map('n','<leader>fb',':<C-U>Leaderf buffer --popup<CR>',opt)
+map('n','<leader>fr',':<C-U>Leaderf mru --popup --absolute-path<CR>',opt)
