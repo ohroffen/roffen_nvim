0090d3fdd5a8259bf3ad335f7c2f36ab5ba05f5a
roffen <fan@example.com>
2022-12-21 22:17:22 +0800

    feat(new-plugin): add which-key

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -115,4 +115,15 @@ return require('packer').startup(function(use)
   use {'onsails/lspkind-nvim'}
   -- cursorword
   use {'yamatsum/nvim-cursorline'}
+  -- which-key
+  use {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
 end)
